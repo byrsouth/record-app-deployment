ae26a0560f466abd8a82d997f93da66cad47c25a
Byron <byron.southerland@icloud.com>
2023-01-21 17:07:52 -0500

    v1.22

--- src/recordDeployment.ts
+++ src/recordDeployment.ts
@@ -8,11 +8,11 @@ import * as httpm from '@actions/http-client'
 
 export async function recordDeployment(deployData: DeployData): Promise<void> {
    const _http: httpm.HttpClient = new httpm.HttpClient();
-   const res: httpm.HttpClientResponse = await _http.post(
+    await _http.post(
       'https://httpappdeployment.azurewebsites.net/api/httpappdeployment',
       JSON.stringify(deployData)
    );
-   core.info(JSON.stringify(res.message));
+   core.info(JSON.stringify(deployData));
 }
 
 

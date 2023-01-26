72913e612a6fa161cd8e8816cb3dc0d378a69822
Byron-Southerland_sasinst <byron.southerland@sas.com>
2023-01-26 09:21:40 -0500

    added timestamp formating

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function run(): Promise<void> {
       tagName: tagName,
       branch: event.repository?.default_branch,
       version: version,
-      timestamp: new Date().getTime(),
+      timestamp: new Date().toUTCString(),
       commit: {
         id: commitData.id,
         userName: commitData.author.username,
